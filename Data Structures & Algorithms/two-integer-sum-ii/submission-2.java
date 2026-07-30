ff9f630a4bc45761c98497a36807852a8f1f20b5
BENIKEN BRAHIM <benikenbrahim432@gmail.com>
2026-07-31 00:20:02 +0100

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        //using pointer
+        int left=0;
+        int right=numbers.length-1;
+        while(true){
+            if( numbers[left]+numbers[right]>target){
+                right--;
+            }
+            else if(numbers[left]+numbers[right]<target){
+                left++;
+            }
+            else{
+                return new int[]{left + 1, right + 1};
+            }
+        }        
+    }
+}
