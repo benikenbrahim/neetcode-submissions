2f12e0e7063aab6e7aa0a2665473b4034cc487b9
BENIKEN BRAHIM <benikenbrahim432@gmail.com>
2026-09-15 00:18:22 +0100

    Add: search-insert-position - submission-5

---
+++ Data Structures & Algorithms/search-insert-position/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+      
+        while(left<=right ){
+            int midle=left+(right-left)/2;
+            if(nums[midle]==target){
+                return midle;
+            }
+            if(nums[midle]<target){
+                left=midle+1;
+            }
+            if(nums[midle]>target){
+                right=midle-1;
+            }
+        }
+       
+        return left;
+    }
+}
